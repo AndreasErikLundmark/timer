bf0edd5c544f35ed4b02a9d52e066a555ef5a34b
Andreas Lundmark <andreas.erik.lundmark@gmail.com>
2025-03-17 11:24:04 +0100

    counter.h

---
+++ headers/counter.h
@@ -0,0 +1,25 @@
+
+#ifndef COUNTER_H
+#define COUNTER_H
+
+
+
+class Counter
+{
+
+public:
+    static create(int minutes, int seconds);
+    virtual void countDown();
+
+protected:
+    Counter(int minutes, int seconds)
+   
+
+private:
+    int minutes;
+    int seconds;
+    
+    
+};
+
+#endif
