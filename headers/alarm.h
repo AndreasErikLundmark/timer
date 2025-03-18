7b9a4bb603342be799b3a0d8e0976cfc12e34193
Andreas Lundmark <andreas.erik.lundmark@gmail.com>
2025-03-18 13:38:55 +0100

    alarm header

---
+++ headers/alarm.h
@@ -0,0 +1,21 @@
+#define ALARM_M
+
+class Alarm
+{
+
+    public:
+     static Alarm* create();
+     void start();
+     void stop();
+    
+
+    protected:
+        Alarm();
+
+
+    private:
+    
+}
+
+
+#endif
